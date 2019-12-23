ff1cdd9205daba4d291b7ab0ca61012e5c88485b
Xuehui Ye <yexuehui@momenta.ai>
2019-12-23 12:22:58 +0800

    sth.

--- src/pages/WelcomeScatter.tsx
+++ src/pages/WelcomeScatter.tsx
@@ -5,7 +5,7 @@ import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import echarts from 'echarts/lib/echarts';
 import 'echarts-gl';
 // import data from '@/data/income.js';
-import data from '@/data/bench.json';
+// import data from '@/data/bench.json';
 
 // import 'echarts/lib/chart/bar';
 // import 'echarts/lib/chart/line';
@@ -21,7 +21,7 @@ export default (): React.ReactNode => {
     useEffect(() => {
         // 初始化
         var myChart = echarts.init(document.getElementById('mainx') as HTMLDivElement);
-        // var data = [[815, 34.05, 351014, "Australia", 1801], [1314, 39, 645526, "Canada", 1802], [985, 32, 402711280, "China", 1853], [1543, 36.26, 1181650, "Cuba", 1855], [1512, 37.35415172, 1607810, "Finland", 1861], [2146, 43.28, 36277905, "France", 1857], [2182, 38.37, 33663143, "Germany", 1881]];
+        var data = [['change', 'period', 'total'], [2, 3, 4], [2.5, 3.5, 4.5]];
         const option = {
             tooltip: {},
             grid3D: {
@@ -32,12 +32,16 @@ export default (): React.ReactNode => {
             },
             xAxis3D: {
                 // 因为 x 轴和 y 轴都是类目数据，所以需要设置 type: 'category' 保证正确显示数据。
-                // type: 'category'
+                type: 'category'
             },
-            yAxis3D: {},
+            yAxis3D: {
+                type: 'category'
+            },
             zAxis3D: {},
             dataset: {
                 dimensions: [
+                    // { name: 'change', type: 'ordinal' as echarts.DimentionType },
+                    // { name: 'period', },
                     'change',
                     'period',
                     'total',
@@ -46,15 +50,15 @@ export default (): React.ReactNode => {
                 ],
                 source: data,//.filter((v: any) => v[1] >= 40)
             },
-            visualMap: {
-                calculable: true, //可以筛选
-                max: 400000,
-                min: 300000,
-                dimension: 'total', // 维度的名字默认就是表头的属性名, 或者列的顺序如第二列
-                inRange: {
-                    color: ['#FF0000', 'blue', 'green']
-                }
-            },
+            // visualMap: {
+            //     calculable: true,
+            //     max: 20,
+            //     // 维度的名字默认就是表头的属性名
+            //     dimension: 'change',
+            //     // inRange: {
+            //     //     color: ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf', '#fee090', '#fdae61', '#f46d43', '#d73027', '#a50026']
+            //     // }
+            // },
             series: [
                 {
                     type: 'scatter3D',
