d0685dad21ddf11bbe7be78e87ed09ff8f631ba7
ahuigo <1781999+ahuigo@users.noreply.github.com>
2023-05-12 22:33:16 +0800

    demo:dom-resize

---
+++ src/pages/dom/event-resize.tsx
@@ -0,0 +1,65 @@
+import { useEffect, useRef, useState } from "react";
+
+const doc = `
+  # body onresize
+  仅body 有效：
+    <body onresize="myFunction()">
+    window.addEventListener("resize", myFunction);
+
+  # element resize
+          1. setInterval: monitor element.offsetWidth; 
+            refer: https://dirask.com/posts/JavaScript-onResize-event-for-div-element-DnKaXp
+
+          2. ResizeObserver
+`;
+export default () => {
+  const [isShow, setShow] = useState(false);
+  const mainRef = useRef<HTMLDivElement>(null);
+
+  const changeShow = () => {
+    setShow(!isShow);
+  };
+
+  useEffect(() => {
+    var observer = new ResizeObserver((entries) => {
+      for (var i = 0; i < entries.length; ++i) {
+        var entry = entries[i];
+        var sizes = entry.borderBoxSize;  // or:  entry.contentBoxSize;
+        if (sizes) {
+          console.log('new:', entry.target.id);
+          for (var j = 0; j < sizes.length; ++j) {
+            var size = sizes[j];
+            console.log('currentWidth=' + size.inlineSize + ' currentHeight=' + size.blockSize);
+          }
+        }
+      }
+    });
+    observer.observe(mainRef.current!);
+    observer.observe(document.querySelector('#main1')!);
+  }, []);
+
+  return (
+    <>
+      <div id="main1" className="bg-orange-100 w-80 h-16 border border-solid resize overflow-auto">resize css</div>
+
+      <div className="h-96 flex">
+        <div id="main2" ref={mainRef} className="main bg-blue-200 flex-1">
+          <button onClick={changeShow} className="text-lg">
+            toggle-size
+          </button>
+
+          <h2>implement onsize ways:</h2>
+          <pre className="whitespace-pre-wrap">
+            {doc}
+          </pre>
+        </div>
+
+        {isShow && (
+          <div className="sidebar flex bg-green-400 w-1/2">
+            <h3>sidebar</h3>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
